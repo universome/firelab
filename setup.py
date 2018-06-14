045b8e2715c6bffb5b6ea1753d532ee334de45c1
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-06-14 12:32:38 +0300

    chore: fix setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='hasheroku',
+    name='firelab',
     version='0.0.1',
     author='Ivan Skorokhodov',
     author_email='iskorokhodov@gmail.com',
     url='https://github.com/universome/firelab',
     description='Experimental framework to run pytorch experiments',
     packages=find_packages(exclude=('tests',)),
-    entry_points = {
-        'console_scripts': ['firelab=firelab.cli:main'],
-    },
+    entry_points = {'console_scripts': ['firelab=firelab.cli:main']},
     license='BSD',
-    python_requires='>=3',
+    python_requires='>=3.4',
     long_description=open('README.md').read(),
     zip_safe=True
 )
