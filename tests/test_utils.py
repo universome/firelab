1d3b2247a70794a60573ed2a4207ab1158dc6f88
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-08-13 14:30:58 +0300

    test: tests for determine_turn util

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+from firelab.utils import determine_turn
+
+
+def test_determine_turn():
+    assert determine_turn(0, [5,2]) == 0
+    assert determine_turn(4, [5,2]) == 0
+    assert determine_turn(5, [5,2]) == 1
+    assert determine_turn(6, [5,2]) == 1
+    assert determine_turn(7, [5,2]) == 0
+    assert determine_turn(13, [3,3,3]) == 1
+    assert determine_turn(8, [3,3,3]) == 2
+    assert determine_turn(9, [3,3,3]) == 0
