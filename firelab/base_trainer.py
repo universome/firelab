e51fe806fdc28a5849ddc8341521924b35741857
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-06-04 12:55:34 +0300

    refactor: rename private methods

--- firelab/base_trainer.py
+++ firelab/base_trainer.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 import torch
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import yaml
 
@@ -11,6 +11,8 @@ from firelab.utils.training_utils import is_history_improving, safe_oom_call
 
 class BaseTrainer:
     def __init__(self, config):
+        # TODO: we should somehow say more loudly that we are reserving these properties
+        # Besides, some properties are vital for user to define at he has not idea about it :|
         self.config = config
         self.num_iters_done = 0
         self.num_epochs_done = 0
@@ -49,24 +51,11 @@ class BaseTrainer:
         self.train_dataloader = None
         self.val_dataloader = None
 
-        self.writer = SummaryWriter(config.firelab.logs_path)
+        self.writer = SummaryWriter(config.firelab.logs_path, flush_secs=5)
 
-    def start(self):
-        if len(self.config.available_gpus) > 0:
-            with torch.cuda.device(self.config.available_gpus[0]):
-                self._start()
-        else:
-            self._start()
-
-    def _start(self):
-        self.init_dataloaders()
-        self.init_models()
-        self.init_criterions()
-        self.init_optimizers()
-        self.load_checkpoint()
-
-        self.run_training()
-
+    ############################
+    ### Overwritable methods ###
+    ############################
     def init_dataloaders(self):
         pass
 
@@ -79,29 +68,6 @@ class BaseTrainer:
     def init_optimizers(self):
         pass
 
-    def run_training(self):
-        try:
-            while not self.should_stop():
-                print('Running epoch #{}'.format(self.num_epochs_done+1))
-
-                for batch in tqdm(self.train_dataloader):
-                    self.train_mode()
-                    safe_oom_call(self.train_on_batch, batch, debug=self.config.get('debug_gpu'))
-
-                    self.num_iters_done += 1
-
-                    # Let's validate without grad enabled (less memory consumption)
-                    with torch.no_grad():
-                        safe_oom_call(self.try_to_validate, debug=self.config.get('debug_gpu'))
-
-                    self.checkpoint()
-
-                self.num_epochs_done += 1
-                self.on_epoch_done()
-        except Exception as e:
-            self.write_summary(str(e))
-            raise
-
     def train_on_batch(self, batch):
         pass
 
@@ -109,7 +75,69 @@ class BaseTrainer:
         "Callback which is called when epoch has beed done"
         pass
 
-    def try_to_validate(self):
+    def validate(self):
+        pass
+
+    def on_training_done(self):
+        pass
+
+    ######################
+    ### Public methods ###
+    ######################
+    def start(self):
+        if len(self.config.available_gpus) > 0:
+            with torch.cuda.device(self.config.available_gpus[0]):
+                self._start()
+        else:
+            self._start()
+
+    def write_losses(self, losses: dict, prefix=''):
+        """
+        Iterates over losses and logs them with self.writer
+        Arguments:
+            - losses: dict of losses; each loss should be a scalar
+        """
+        for k in losses:
+            self.writer.add_scalar(prefix + k, losses[k], self.num_iters_done)
+
+    #######################
+    ### Private methods ###
+    #######################
+    def _start(self):
+        self.init_dataloaders()
+        self.init_models()
+        self.init_criterions()
+        self.init_optimizers()
+        self._try_to_load_checkpoint()
+
+        self._run_training()
+        self.on_training_done()
+        self.writer.close()
+
+    def _run_training(self):
+        try:
+            while not self._should_stop():
+                print('Running epoch #{}'.format(self.num_epochs_done+1))
+
+                for batch in tqdm(self.train_dataloader):
+                    self._train_mode()
+                    safe_oom_call(self.train_on_batch, batch, debug=self.config.get('debug_gpu'))
+
+                    self.num_iters_done += 1
+
+                    # Let's validate without grad enabled (less memory consumption)
+                    with torch.no_grad():
+                        safe_oom_call(self._try_to_validate, debug=self.config.get('debug_gpu'))
+
+                    self._checkpoint()
+
+                self.num_epochs_done += 1
+                self.on_epoch_done()
+        except Exception as e:
+            self._write_summary(str(e))
+            raise
+
+    def _try_to_validate(self):
         should_validate = False
 
         if self.val_freq_iters:
@@ -121,13 +149,11 @@ class BaseTrainer:
             should_validate = was_epoch_just_finished and is_epoch_appropriate
 
         if should_validate:
-            self.eval_mode()
+            self._eval_mode()
             self.validate()
 
-    def validate(self):
-        pass
 
-    def checkpoint(self):
+    def _checkpoint(self):
         should_checkpoint = False
 
         if self.checkpoint_freq_iters:
@@ -141,22 +167,22 @@ class BaseTrainer:
         if not should_checkpoint: return
 
         for module_name in self.checkpoint_list:
-            self.save_module_state(getattr(self, module_name), module_name)
+            self._save_module_state(getattr(self, module_name), module_name)
 
         if self.config.checkpoint.get('pickle'):
             for attr in self.config.checkpoint.pickle:
-                self.pickle(getattr(self, attr), attr)
+                self._pickle(getattr(self, attr), attr)
 
-        self.checkpoint_freq_warning()
+        self._checkpoint_freq_warning()
 
-    def checkpoint_freq_warning(self):
+    def _checkpoint_freq_warning(self):
         """
         Prints warning if we write checkpoints too often
         TODO: wip
         """
         pass
 
-    def load_checkpoint(self):
+    def _try_to_load_checkpoint(self):
         "Loads model state from checkpoint if it is provided"
         if self.config.firelab.get('continue_from_iter') is None: return
 
@@ -165,29 +191,29 @@ class BaseTrainer:
             self.num_epochs_done = self.num_iters_done // len(self.train_dataloader)
 
         for module_name in self.checkpoint_list:
-            self.load_module_state(getattr(self, module_name), module_name, self.config.firelab.continue_from_iter)
+            self._load_module_state(getattr(self, module_name), module_name, self.config.firelab.continue_from_iter)
 
         if self.config.checkpoint.get('pickle'):
             for module_name in self.config.checkpoint.pickle:
-                self.unpickle(module_name, self.config.firelab.continue_from_iter)
+                self._unpickle(module_name, self.config.firelab.continue_from_iter)
 
-    def should_stop(self) -> bool:
+    def _should_stop(self) -> bool:
         "Checks all stopping criteria"
         if self.max_num_iters and self.num_iters_done >= self.max_num_iters:
-            self.write_summary('Max num iters exceeded')
+            self._write_summary('Max num iters exceeded')
             return True
 
         if self.max_num_epochs and self.num_epochs_done >= self.max_num_epochs:
-            self.write_summary('Max num epochs exceeded')
+            self._write_summary('Max num epochs exceeded')
             return True
 
-        if self.should_early_stop():
-            self.write_summary('Early stopping')
+        if self._should_early_stop():
+            self._write_summary('Early stopping')
             return True
 
         return False
 
-    def should_early_stop(self):
+    def _should_early_stop(self):
         "Checks early stopping criterion"
         if self.config.get('early_stopping') is None: return False
 
@@ -197,54 +223,54 @@ class BaseTrainer:
 
         return not is_history_improving(history, n_steps, should_decrease)
 
-    def train_mode(self):
+    # TODO: we can gather modules automaticall (via "isinstance")
+    def _train_mode(self):
         "Switches all models into training mode"
+
+        if not self.config.has('modules'): return
+        if not self.config.modules.has('models'): return
+
         for model_name in self.config.modules.models:
             getattr(self, model_name).train()
 
-    def eval_mode(self):
+    def _eval_mode(self):
         "Switches all models into evaluation mode"
+
+        if not self.config.has('modules'): return
+        if not self.config.modules.has('models'): return
+
         for model_name in self.config.modules.models:
             getattr(self, model_name).eval()
 
-    def save_module_state(self, module, name):
-        module_name = '{}-{}.pth'.format(name, self.num_iters_done)
+    def _save_module_state(self, module, name):
+        module_name = '{}-{}.pt'.format(name, self.num_iters_done)
         module_path = os.path.join(self.config.firelab.checkpoints_path, module_name)
         torch.save(module.state_dict(), module_path)
 
-    def load_module_state(self, module, name, iteration):
-        module_name = '{}-{}.pth'.format(name, iteration)
+    def _load_module_state(self, module, name, iteration):
+        module_name = '{}-{}.pt'.format(name, iteration)
         module_path = os.path.join(self.config.firelab.checkpoints_path, module_name)
         module.load_state_dict(torch.load(module_path))
 
-    def pickle(self, module, name):
+    def _pickle(self, module, name):
         file_name = '{}-{}.pickle'.format(name, self.num_iters_done)
         path = os.path.join(self.config.firelab.checkpoints_path, file_name)
         pickle.dump(module, open(path, 'wb'))
 
-    def unpickle(self, name, iteration):
-        setattr(self, name, self.read_pickle_module(name, iteration))
+    def _unpickle(self, name, iteration):
+        setattr(self, name, self._read_pickle_module(name, iteration))
 
-    def read_pickle_module(self, name, iteration):
+    def _read_pickle_module(self, name, iteration):
         file_name = '{}-{}.pickle'.format(name, iteration)
         path = os.path.join(self.config.firelab.checkpoints_path, file_name)
 
         return pickle.load(open(path, 'rb'))
 
-    def write_losses(self, losses: dict, prefix=''):
-        """
-        Iterates over losses and logs them with self.writer
-        Arguments:
-            - losses: dict of losses; each loss should be a scalar
-        """
-        for k in losses:
-            self.writer.add_scalar(prefix + k, losses[k], self.num_iters_done)
-
-    def write_summary(self, termination_reason:str):
+    def _write_summary(self, termination_reason:str):
         print('Terminating experiment because [%s]' % termination_reason)
 
         summary = {
-            'name': self.config.firelab.exp_subname,
+            'name': self.config.firelab.exp_name,
             'termination_reason': termination_reason,
             'num_iters_done': self.num_iters_done,
             'num_epochs_done': self.num_epochs_done,
